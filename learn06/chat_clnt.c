982f49c3725c5ab24b511a35aa5177d7f05112be
breadcoffee <146920372+breadcoffee@users.noreply.github.com>
2024-06-18 17:31:38 +0900

    ..

--- learn06/chat_clnt.c
+++ learn06/chat_clnt.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
-	
+
 #define BUF_SIZE 100
 #define NAME_SIZE 20
-	
+
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
-	
+
 char name[NAME_SIZE]="[DEFAULT]";
 char msg[BUF_SIZE];
-	
+
 int main(int argc, char *argv[])
 {
 	int sock;
@@ -26,35 +26,35 @@ int main(int argc, char *argv[])
 		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
 		exit(1);
 	 }
-	
+
 	sprintf(name, "[%s]", argv[3]);
 	sock=socket(PF_INET, SOCK_STREAM, 0);
-	
+
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
 	serv_addr.sin_port=htons(atoi(argv[2]));
-	  
+
 	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
 		error_handling("connect() error");
-	
+
 	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
 	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
 	pthread_join(snd_thread, &thread_return);
 	pthread_join(rcv_thread, &thread_return);
-	close(sock);  
+	close(sock);
 	return 0;
 }
-	
+
 void * send_msg(void * arg)   // send thread main
 {
 	int sock=*((int*)arg);
 	char name_msg[NAME_SIZE+BUF_SIZE];
-	while(1) 
+	while(1)
 	{
-        printf("enter > ");
+	        //printf("enter > ");
 		fgets(msg, BUF_SIZE, stdin);
-		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) 
+		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n"))
 		{
 			close(sock);
 			exit(0);
@@ -64,7 +64,7 @@ void * send_msg(void * arg)   // send thread main
 	}
 	return NULL;
 }
-	
+
 void * recv_msg(void * arg)   // read thread main
 {
 	int sock=*((int*)arg);
@@ -73,14 +73,14 @@ void * recv_msg(void * arg)   // read thread main
 	while(1)
 	{
 		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
-		if(str_len==-1) 
+		if(str_len==-1)
 			return (void*)-1;
 		name_msg[str_len]=0;
 		fputs(name_msg, stdout);
 	}
 	return NULL;
 }
-	
+
 void error_handling(char *msg)
 {
 	fputs(msg, stderr);
