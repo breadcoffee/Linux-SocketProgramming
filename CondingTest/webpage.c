5efbadba8234249f0a40a215a86e403bd81f9bca
breadcoffee <printf0543@gmail.com>
2024-06-19 14:12:12 +0900

    Update webpage.c

--- CondingTest/webpage.c
+++ CondingTest/webpage.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include <fcntl.h>
 #include<arpa/inet.h>
-#include<sys/sendfile.h>
+#include<unistd.h>
 #include<sys/socket.h>
+#include<sys/stat.h>
+#include <fcntl.h>
+#include<sys/sendfile.h>
 
 #define BUF_SIZE 1024
 #define TRUE 1
