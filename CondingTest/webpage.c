3f60ab4a2a100698880bbbcc361ff4fc9634ce43
breadcoffee <printf0543@gmail.com>
2024-06-19 11:45:46 +0900

    Update webpage.c

--- CondingTest/webpage.c
+++ CondingTest/webpage.c
@@ -1,198 +1,159 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
+#include <string.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
 
-#define BUF_SIZE 1000
-#define HEADER_FMT "HTTP/1.1 %d %s\nContent-Length: %ld\nContent-Type: %s\n\n"
+#define BUF_SIZE 1024
+#define SMALL_BUF 100
 
-#define NOT_FOUND_CONTENT       "<h1>404 Not Found</h1>\n"
-#define SERVER_ERROR_CONTENT    "<h1>500 Internal Server Error</h1>\n"
+void* request_handler(void* arg);
+void send_data(FILE* fp, char* ct, char* file_name);
+char* content_type(char* file);
+void send_error(FILE* fp);
+void error_handling(char* message);
 
-/*
-    @func   assign address to the created socket lsock(sd)
-    @return bind() return value
-*/
-int bind_lsock(int lsock, int port) {
-    struct sockaddr_in sin;
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock;
+	struct sockaddr_in serv_adr, clnt_adr;
+	int clnt_adr_size;
+	char buf[BUF_SIZE];
+	pthread_t t_id;	
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	if(listen(serv_sock, 20)==-1)
+		error_handling("listen() error");
 
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
-    sin.sin_port = htons(port);
-
-    return bind(lsock, (struct sockaddr *)&sin, sizeof(sin));
+	while(1)
+	{
+		clnt_adr_size=sizeof(clnt_adr);
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_size);
+		printf("Connection Request : %s:%d\n", 
+			inet_ntoa(clnt_adr.sin_addr), ntohs(clnt_adr.sin_port));
+		pthread_create(&t_id, NULL, request_handler, &clnt_sock);
+		pthread_detach(t_id);
+	}
+	close(serv_sock);
+	return 0;
 }
 
-/* ...skip: codes related to HTTP... */
+void* request_handler(void *arg)
+{
+	int clnt_sock=*((int*)arg);
+	char req_line[SMALL_BUF];
+	FILE* clnt_read;
+	FILE* clnt_write;
+	
+	char method[10];
+	char ct[15];
+	char file_name[30];
+  
+	clnt_read=fdopen(clnt_sock, "r");
+	clnt_write=fdopen(dup(clnt_sock), "w");
+	fgets(req_line, SMALL_BUF, clnt_read);	
+	if(strstr(req_line, "HTTP/")==NULL)
+	{
+		send_error(clnt_write);
+		fclose(clnt_read);
+		fclose(clnt_write);
+		return;
+	 }
+	
+	strcpy(method, strtok(req_line, " /"));
+	strcpy(file_name, strtok(NULL, " /"));
+	strcpy(ct, content_type(file_name));
+	if(strcmp(method, "GET")!=0)
+	{
+		send_error(clnt_write);
+		fclose(clnt_read);
+		fclose(clnt_write);
+		return;
+	 }
 
-int main(int argc, char **argv) {
-    int port, pid;
-    int lsock, asock;
-
-    struct sockaddr_in remote_sin;
-    socklen_t remote_sin_len;
-
-    if (argc < 2) {
-        printf("Usage: \n");
-        printf("\t%s {port}: runs mini HTTP server.\n", argv[0]);
-        exit(0);
-    }
-
-    port = atoi(argv[1]);
-    printf("[INFO] The server will listen to port: %d.\n", port);
-
-    lsock = socket(AF_INET, SOCK_STREAM, 0);
-    if (lsock < 0) {
-        perror("[ERR] failed to create lsock.\n");
-        exit(1);
-    }
-
-    if (bind_lsock(lsock, port) < 0) {
-        perror("[ERR] failed to bind lsock.\n");
-        exit(1);
-    }
-
-    if (listen(lsock, 10) < 0) {
-        perror("[ERR] failed to listen lsock.\n");
-        exit(1);
-    }
-
-    // to handle zombie process
-    signal(SIGCHLD, SIG_IGN);
-
-    while (1) {
-        printf("[INFO] waiting...\n");
-        asock = accept(lsock, (struct sockaddr *)&remote_sin, &remote_sin_len);
-        if (asock < 0) {
-            perror("[ERR] failed to accept.\n");
-            continue;
-        }
-
-        pid = fork();
-        if (pid == 0) {
-            close(lsock); http_handler(asock); close(asock);
-            exit(0);
-        }
-
-        if (pid != 0)   { close(asock); }
-        if (pid < 0)    { perror("[ERR] failed to fork.\n"); }
-    }
-}
-/*
-    @func   format HTTP header by given params
-    @return
-*/
-void fill_header(char *header, int status, long len, char *type) {
-    char status_text[40];
-    switch (status) {
-        case 200:
-            strcpy(status_text, "OK"); break;
-        case 404:
-            strcpy(status_text, "Not Found"); break;
-        case 500:
-        default:
-            strcpy(status_text, "Internal Server Error"); break;
-    }
-    sprintf(header, HEADER_FMT, status, status_text, len, type);
+	fclose(clnt_read);
+	send_data(clnt_write, ct, file_name); 
 }
 
-/*
-    @func   find content type from uri
-    @return
-*/
-void find_mime(char *ct_type, char *uri) {
-    char *ext = strrchr(uri, '.');
-    if (!strcmp(ext, ".html"))
-        strcpy(ct_type, "text/html");
-    else if (!strcmp(ext, ".jpg") || !strcmp(ext, ".jpeg"))
-        strcpy(ct_type, "images/jpeg");
-    else if (!strcmp(ext, ".png"))
-        strcpy(ct_type, "images/png");
-    else if (!strcmp(ext, ".css"))
-        strcpy(ct_type, "text/css");
-    else if (!strcmp(ext, ".js"))
-        strcpy(ct_type, "text/javascript");
-    else strcpy(ct_type, "text/plain");
+void send_data(FILE* fp, char* ct, char* file_name)
+{
+	char protocol[]="HTTP/1.0 200 OK\r\n";
+	char server[]="Server:Linux Web Server \r\n";
+	char cnt_len[]="Content-length:2048\r\n";
+	char cnt_type[SMALL_BUF];
+	char buf[BUF_SIZE];
+	FILE* send_file;
+	
+	sprintf(cnt_type, "Content-type:%s\r\n\r\n", ct);
+	send_file=fopen(file_name, "r");
+	if(send_file==NULL)
+	{
+		send_error(fp);
+		return;
+	}
+
+	/* 헤더 정보 전송 */
+	fputs(protocol, fp);
+	fputs(server, fp);
+	fputs(cnt_len, fp);
+	fputs(cnt_type, fp);
+
+	/* 요청 데이터 전송 */
+	while(fgets(buf, BUF_SIZE, send_file)!=NULL) 
+	{
+		fputs(buf, fp);
+		fflush(fp);
+	}
+	fflush(fp);
+	fclose(fp);
 }
 
-/*
-    @func handler for not found
-    @return
-*/
-void handle_404(int asock) {
-    char header[BUF_SIZE];
-    fill_header(header, 404, sizeof(NOT_FOUND_CONTENT), "text/html");
-
-    write(asock, header, strlen(header));
-    write(asock, NOT_FOUND_CONTENT, sizeof(NOT_FOUND_CONTENT));
+char* content_type(char* file)
+{
+	char extension[SMALL_BUF];
+	char file_name[SMALL_BUF];
+	strcpy(file_name, file);
+	strtok(file_name, ".");
+	strcpy(extension, strtok(NULL, "."));
+	
+	if(!strcmp(extension, "html")||!strcmp(extension, "htm")) 
+		return "text/html";
+	else
+		return "text/plain";
 }
 
-/*
-    @func handler for internal server error
-    @return
-*/
-void handle_500(int asock) {
-    char header[BUF_SIZE];
-    fill_header(header, 500, sizeof(SERVER_ERROR_CONTENT), "text/html");
+void send_error(FILE* fp)
+{	
+	char protocol[]="HTTP/1.0 400 Bad Request\r\n";
+	char server[]="Server:Linux Web Server \r\n";
+	char cnt_len[]="Content-length:2048\r\n";
+	char cnt_type[]="Content-type:text/html\r\n\r\n";
+	char content[]="<html><head><title>NETWORK</title></head>"
+	       "<body><font size=+5><br>오류 발생! 요청 파일명 및 요청 방식 확인!"
+		   "</font></body></html>";
 
-    write(asock, header, strlen(header));
-    write(asock, SERVER_ERROR_CONTENT, sizeof(SERVER_ERROR_CONTENT));
+	fputs(protocol, fp);
+	fputs(server, fp);
+	fputs(cnt_len, fp);
+	fputs(cnt_type, fp);
+	fflush(fp);
 }
 
-/*
-    @func main http handler; try to open and send requested resource, calls error handler on failure
-    @return
-*/
-void http_handler(int asock) {
-    char header[BUF_SIZE];
-    char buf[BUF_SIZE];
-
-    if (read(asock, buf, BUF_SIZE) < 0) {
-        perror("[ERR] Failed to read request.\n");
-        handle_500(asock); return;
-    }
-
-    char *method = strtok(buf, " ");
-    char *uri = strtok(NULL, " ");
-    if (method == NULL || uri == NULL) {
-        perror("[ERR] Failed to identify method, URI.\n");
-        handle_500(asock); return;
-    }
-
-    printf("[INFO] Handling Request: method=%s, URI=%s\n", method, uri);
-
-    char safe_uri[BUF_SIZE];
-    char *local_uri;
-    struct stat st;
-
-    strcpy(safe_uri, uri);
-    if (!strcmp(safe_uri, "/")) strcpy(safe_uri, "/index.html");
-
-    local_uri = safe_uri + 1;
-    if (stat(local_uri, &st) < 0) {
-        perror("[WARN] No file found matching URI.\n");
-        handle_404(asock); return;
-    }
-
-    int fd = open(local_uri, O_RDONLY);
-    if (fd < 0) {
-        perror("[ERR] Failed to open file.\n");
-        handle_500(asock); return;
-    }
-
-    int ct_len = st.st_size;
-    char ct_type[40];
-    find_mime(ct_type, local_uri);
-    fill_header(header, 200, ct_len, ct_type);
-    write(asock, header, strlen(header));
-
-    int cnt;
-    while ((cnt = read(fd, buf, BUF_SIZE)) > 0)
-        write(asock, buf, cnt);
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
